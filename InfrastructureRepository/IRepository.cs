52879bbfd9130b3b707002d79fb92d1275df90ea
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-30 18:46:31 +0200

    create interface irepozitory

---
+++ InfrastructureRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using HospitalOnionArchitecture.Domain.Core;
+using System.Collections.Generic;
+
+namespace InfrastructureRepository
+{
+    interface IRepository<T> where T : User
+    {
+        IEnumerable<T> GetAllAsync();
+        T GetByIdAsync(int id);
+        void Insert(T user);
+        void UpdateDb(T user);
+        void Delete(T user);
+        void Save();
+    }
