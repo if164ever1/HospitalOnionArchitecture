3c99019665d79717185e200a56d3db93103a0b00
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-30 18:09:28 +0200

    create domain with user class

---
+++ Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalOnionArchitecture.Domain.Core
+{
+    public class User
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
