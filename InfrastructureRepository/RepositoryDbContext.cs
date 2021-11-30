59398ac5354a6a6d5745e606e5eaa00a3773d002
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-30 18:21:42 +0200

    create class repository db context

---
+++ InfrastructureRepository/RepositoryDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using HospitalOnionArchitecture.Domain.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfrastructureRepository
+{
+    class RepositoryDbContext: DbContext
+    {
+        public RepositoryDbContext(DbContextOptions<RepositoryDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
