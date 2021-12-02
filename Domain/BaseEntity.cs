d7fe87045b184848b81cd1787db63a763ec32a06
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-02 10:07:54 +0200

    add class baseentity

---
+++ Domain/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalOnionArchitecture.Domain.Core
+{
+    public class BaseEntity
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime RegistrationDate
+        {   
+            get => RegistrationDate.Date;
+            set 
+            {
+                RegistrationDate = DateTime.Today.Date;
+            }
+        }
+    }
+}
