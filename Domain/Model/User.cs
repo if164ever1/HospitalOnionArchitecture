71e313bb85b3618084885665b7432ca6eb687032
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-02 12:34:52 +0200

    delete usermap

---
+++ Domain/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalOnionArchitecture.Domain.Core
+{
+    public class User
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        //public virtual UserProfile UserProfile { get; set; }
+    }
+}
---
+++ Domain/Model/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using HospitalOnionArchitecture.Domain.Core;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class UserProfile
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int IdUserRef { get; set; }
+        public User User { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
