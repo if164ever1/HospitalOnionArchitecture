b6320395677829518e8963d9e076fd8f5f0f83b6
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-02 10:10:01 +0200

    add class patient

--- Domain/Patient.cs
+++ Domain/Patient.cs
@@ -1,7 +1,14 @@
-﻿
+﻿using HospitalOnionArchitecture.Domain.Core;
+using System;
+
 namespace Domain
 {
     class Patient
     {
+        public User User { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+
     }
 }
