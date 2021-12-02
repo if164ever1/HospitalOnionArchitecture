551e50678b5cea3205809fd43253bada0fc87d48
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-02 12:37:00 +0200

    initial class register

---
+++ ServiceAcount/Register.cs
@@ -0,0 +1,51 @@
+﻿using Domain.MapModel;
+using InfrastructureRepository;
+using ServiceUser;
+using System;
+using System.Linq;
+
+namespace ServiceAcount
+{
+    public class Register
+    {
+        private IUserService userService;
+        private RepositoryDbContext dbContext;
+        public Register(IUserService service, RepositoryDbContext context)
+        {
+            dbContext = context;
+            userService = service;
+        }
+
+        public void Registration(UserMap userMap)
+        {
+            //if (dbContext.Users.FirstOrDefault(db => db.Email == userMap.) != null)
+            //{
+
+            //}
+
+
+
+            //if (userService.FirstOrDefault(u => u.Email == registerRequest.Email) != null)
+            //{
+            //    return BadRequest(new { message = "Login already use" });
+            //}
+
+            //UserModel userModel = new UserModel
+            //{
+            //    FirstName = registerRequest.FirstName,
+            //    LastName = registerRequest.LastName,
+            //    Email = registerRequest.Email,
+            //    Password = CustomHash.HashPassword(registerRequest.Password)
+            //};
+
+            //userContext.Users.Add(userModel);
+            //await userContext.SaveChangesAsync();
+
+            //return Ok(new { message = "Registration is succesfuly" });
+        }
+
+
+
+
+    }
+}
