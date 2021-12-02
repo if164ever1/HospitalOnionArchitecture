fca2f87672c99ccab6dbd03412418e67ee81e175
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-02 12:55:15 +0200

    add project with passhash

---
+++ PassHashService/CustomHash.cs
@@ -0,0 +1,19 @@
+﻿using EasyEncryption;
+
+namespace ServiceAcount.PassHash
+{
+    public static class CustomHash
+    {
+		public static string HashPassword(string input)
+		{ 
+			return SHA.ComputeSHA256Hash(input);
+		}
+
+		public static bool PasswordCheck(string userInput, string savedInDbPassword)
+		{ 
+			string hashedInput = HashPassword(userInput);
+			bool doPasswordsMatch = string.Equals(hashedInput, savedInDbPassword);
+			return doPasswordsMatch;
+		}
+	}
+}
