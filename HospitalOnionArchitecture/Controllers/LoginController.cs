c925d965671b8afcd6893b0d42e1a96391d4ae80
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-05 20:06:51 +0200

    create loginController

---
+++ HospitalOnionArchitecture/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Domain.RequestModel;
+using InfrastructureRepository;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAcount;
+using ServiceUser;
+
+namespace HospitalOnionArchitecture.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly Register register;
+        private readonly Login login;
+        public LoginController(IUserService service, RepositoryDbContext context)
+        {
+            register = new Register(service, context);
+            login = new Login(service, context);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequestUser loginRequest)
+        {
+            if (login.LoginUser(loginRequest))
+            {
+                return Ok("Login successfull");
+            }
+            return BadRequest("Email or password is incorect");
+        }
+    }
+}
