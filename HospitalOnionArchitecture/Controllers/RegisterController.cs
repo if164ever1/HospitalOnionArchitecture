63bb586837bd3fbc7cd2955aa0f7d3208d91237b
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-05 20:07:02 +0200

    create registerController

---
+++ HospitalOnionArchitecture/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using Domain.NewFolder;
+using InfrastructureRepository;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAcount;
+using ServiceUser;
+
+namespace HospitalOnionArchitecture.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class RegisterController : Controller
+    {
+        private readonly Register register;
+        private readonly Login login;
+        public RegisterController(IUserService service, RepositoryDbContext context)
+        {
+            register = new Register(service, context);
+            login = new Login(service, context);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Put(RequestUser user)
+        {
+
+            if (register.Registration(user))
+            {
+                return Ok("Registration successfull");
+            }
+            return BadRequest("Email is already use");
+        }
+    }
+}
