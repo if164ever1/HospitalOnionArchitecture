ce76f34ae0c39585c85555b96fcb2bafce50f17f
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-05 19:56:58 +0200

    create class tokenmodel

---
+++ Domain/Model/TokenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Model
+{
+    public class TokenModel
+    {
+        public int Id { get; set; }
+        public int IdUserRef { get; set; }
+        public string  Token { get; set; }
+    }
+}
